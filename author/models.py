05c633e069b92cd18ac20c418199c12bf7778093
JorgeMano <jorge.l.manosalva@gmail.com>
2022-05-23 19:52:16 -0500

    Author.model reload

--- author/models.py
+++ author/models.py
@@ -1,8 +1,9 @@
 from django.db import models
 
 class Author(models.Model):
-    name = models.CharField(max_length=255)
-    description = models.CharField(max_length=255)
+    firstName = models.CharField(max_length=255)
+    lastName = models.CharField(max_length=255)
+    genre = models.CharField(max_length=255)
 
     def __str__(self):
-        return f'{self.name}'
+        return f'{self.firstName} {self.lastName}'
